498719fca6d7f9650687c752bb67e76b2a51e203
eschala <eduar.chala28@outlook.com>
2023-09-20 20:42:57 -0500

    FALTA FORMULAS

--- CalculadoraBeats_JS.js
+++ CalculadoraBeats_JS.js
@@ -4,7 +4,8 @@ let Tempo, Tiempo, Movimientos, Duracion_Moves,bpm,mv,Compases, operacion;
 let Redonda,Blanca,Negra,Corchea,Semicorchea,Fusa,Semifusa;
 var respuesta;
 alert("Escriba que desea calcular");
-alert("1 Calcular bpm"+"</br>"+
+alert(
+"1 Calcular bpm"+"</br>"+
 "2 Calcular Tiempo"+"<br>"+
 "3 Calcular Golpes o compases"+"<br>"+
 "4 Calcular duracion de movimientos"+"<br/>"+
--- CalculadoraBeats_JS_2.js
+++ CalculadoraBeats_JS_2.js
@@ -17,7 +17,7 @@ function enviar() {
     if (optionInput0.checked == true) {
       displayViewFORM0.style.display = "block";
 
-  /*     alert("Selcionada la opcion para calcular el ritmo"); */
+      /*     alert("Selcionada la opcion para calcular el ritmo"); */
       optionInput = 1;
     } else {
       displayViewFORM0.style.display = "none";
@@ -25,7 +25,7 @@ function enviar() {
     if (optionInput1.checked == true) {
       displayViewFORM1.style.display = "block";
 
-/*       alert("Selcionada la opcion para calcular el tiempo"); */
+      /*       alert("Selcionada la opcion para calcular el tiempo"); */
       optionInput = 2;
     } else {
       displayViewFORM1.style.display = "none";
@@ -33,7 +33,7 @@ function enviar() {
     if (optionInput2.checked == true) {
       displayViewFORM2.style.display = "block";
 
-/*       alert("Selcionada la opcion para calcular los compaces"); */
+      /*       alert("Selcionada la opcion para calcular los compaces"); */
       optionInput = 3;
     } else {
       displayViewFORM2.style.display = "none";
@@ -41,7 +41,7 @@ function enviar() {
     if (optionInput3.checked == true) {
       displayViewFORM3.style.display = "block";
 
-/*       alert("Selcionada la opcion para calcular la duracion de movimientos"); */
+      /*       alert("Selcionada la opcion para calcular la duracion de movimientos"); */
       optionInput = 4;
     } else {
       displayViewFORM3.style.display = "none";
@@ -49,12 +49,132 @@ function enviar() {
     if (optionInput4.checked == true) {
       displayViewFORM4.style.display = "block";
 
-/*       alert("Selcionada la opcion para calcular la cantidad de movimientos"); */
+      /*       alert("Selcionada la opcion para calcular la cantidad de movimientos"); */
       optionInput = 5;
     } else {
       displayViewFORM4.style.display = "none";
       /* alert("que paso mijo"); */
     }
   }
-  
+}
+
+function changeColor() {
+  let colorH3 = document.getElementById("result-forRitmo");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+}
+function changeColor2() {
+  let colorH3 = document.getElementById("result-forTiempo");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+}
+function changeColor3() {
+  let colorH3 = document.getElementById("result-forCompaces");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+}
+function changeColor4() {
+  let colorH3 = document.getElementById("result-forDuracionMovimiento");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+}
+function changeColor5() {
+  let colorH3 = document.getElementById("result-forMovimientos");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+}
+function changeColor() {
+  var colorH3 = document.getElementById("result-forRitmo");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+
+  colorH3 = document.getElementById("result-forTiempo");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+
+  colorH3 = document.getElementById("result-forCompaces");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+
+  colorH3 = document.getElementById("result-forDuracionMovimiento");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+
+  colorH3 = document.getElementById("result-forMovimientos");
+  colorH3.style.color = "black";
+  colorH3.style.fontWeight = "700";
+}
+
+function calcularRITMO() {
+  /* Ritmo/Tempo/Bpm */
+
+  let Compases = document.getElementById("beat-forRitmo").value;
+  let Tiempo = document.getElementById("time-forRitmo").value;
+
+  operacion = 0;
+  document.getElementById("result-forRitmo").innerHTML =
+    operacion + "Bpm o " + operacion + " Golpes por Minuto";
+}
+
+function calcularTIEMPO() {
+  /* Tiempo */
+  let Tempo = document.getElementById("bpm-forTiempo").value;
+  let Compases = document.getElementById("beat-forTiempo").value;
+
+  operacion = 0;
+  if (operacion == 1) {
+    document.getElementById("result-forTiempo").innerHTML =
+      operacion + " minuto o " + operacion * 60 + " segundos";
+  } else {
+    document.getElementById("result-forTiempo").innerHTML =
+      operacion + " minutos o " + operacion * 60 + " segundos";
+  }
+}
+
+function calcularCOMPASES() {
+  /* Compases */
+  let Tempo = document.getElementById("bpm-forCompaces").value;
+  let Tiempo = document.getElementById("time-forCompaces").value;
+
+  operacion = 0;
+  document.getElementById("result-forCompaces").innerHTML = operacion;
+}
+
+function calcularTiempoMOVIMIENTO() {
+  /* Duracion Movimiento */
+  let Compases = document.getElementById("beat-forDuracionMovimiento").value;
+  let Tiempo = document.getElementById("time-forDuracionMovimiento").value;
+
+  operacion = 0;
+  document.getElementById("result-forDuracionMovimiento").innerHTML = operacion;
+}
+
+function calcularMOVIMIENTO() {
+  /* Movimientos */
+  let Compases = document.getElementById("beat-forMovimientos").value;
+  let Tiempo = document.getElementById("time-forMovimientos").value;
+
+  operacion = 0;
+  document.getElementById("result-forMovimientos").innerHTML = operacion;
+}
+
+function changeColor_calcularRITMO() {
+  changeColor();
+  calcularRITMO();
+}
+function changeColor_calcularTIEMPO() {
+  changeColor2();
+  calcularTIEMPO();
+}
+function changeColor_calcularCOMPASES() {
+  changeColor3();
+  calcularCOMPASES();
+}
+function changeColor_calcularTMOVES() {
+  changeColor4();
+  calcularTiempoMOVIMIENTO();
+}
+function changeColor_calcularMOVES() {
+  changeColor5();
+  calcularMOVIMIENTO();
 }
