c7c4276f106479b6cf11ecde78401ee19447e0f7
eschala <eduar.chala28@outlook.com>
2023-10-02 17:23:59 -0500

    Clase_3

---
+++ Clase_3_JS.js
@@ -0,0 +1,23 @@
+/* alert("Hola, pendejito");
+
+let list = [];
+
+
+document.write(list);
+
+
+
+let num1= prompt("cuantos datos quiere qque tenga la lista");
+
+for (i=0;i<num1;i++){
+
+    list[i]=prompt("escriba algo");
+
+
+}
+
+document.write(list); */
+
+
+
+let contenedor = document.querySelector(".Arroz");
